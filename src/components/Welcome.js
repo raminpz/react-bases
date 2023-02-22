b37aa1c13b9cbe2edd3e0479ca195efbf2103e40
raminpz <raminp.1012@gmail.com>
2023-02-22 17:31:42 -0500

    Avance

---
+++ src/components/Welcome.js
@@ -0,0 +1,36 @@
+import { useState, useEffect } from "react";
+
+
+
+
+const Welcome = (props) => {
+    const [counter, setCounter] = useState(0);
+    const [semaforo, setSemaforo] = useState(false);
+    const {message, name} = props;
+
+    useEffect(()=>{
+        console.log(semaforo)
+    },[semaforo])
+
+    const contar = ()=>{
+        setCounter(counter + 1);
+        setSemaforo(!semaforo);
+    }
+    
+
+    return (
+        <>
+            <h1>Hola, {name}</h1> 
+            <h2>Contador de React con Hooks</h2>
+            <h4>EL numero del contador es : {counter}</h4>
+            <p>El semaforo está en color {semaforo ? 'red': 'green'}</p>
+            <button type="submit" onClick={contar}>
+                Sumar Contador
+            </button>   
+                 
+        </> 
+        
+    );
+}
+ 
+export default Welcome;
